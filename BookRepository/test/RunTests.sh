758c89d8dec09d78fadddf0421b43c4a780a7628
zyake <zyake@zyake-ThinkPad-E420>
2012-11-04 16:27:07 +0900

    fix error

--- BookRepository/test/RunTests.sh
+++ BookRepository/test/RunTests.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd BookRepository/test
 $PHANTOMJS_HOME/bin/phantomjs $PHANTOMJS_HOME/examples/run-qunit.js AssertUtilsTest.html
 $PHANTOMJS_HOME/bin/phantomjs $PHANTOMJS_HOME/examples/run-qunit.js SelectJsonMapperTest.html
