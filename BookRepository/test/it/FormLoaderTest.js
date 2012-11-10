7836805ee066df8309d718718a4563197da646d8
zyake <zyake@zyake-ThinkPad-E420>
2012-11-10 17:17:08 +0900

    fix test

--- BookRepository/test/it/FormLoaderTest.js
+++ BookRepository/test/it/FormLoaderTest.js
@@ -3,24 +3,15 @@
  */
 asyncTest("fetch01", function() {
   var selectMapper = new SelectJsonMapper();
-  var sleepSelectMapper = new SelectJsonMapper();
-  sleepSelectMapper.target = selectMapper;
-  sleepSelectMapper.mapToElement = function(json, addTo) {
-    for ( var i = 0 ; i < 9999 ; i ++ ) {
-      for ( var j = 0 ; j < 99999 ; j ++ ) {
-      }
-    }
-    this.target.mapToElement(json, addTo);
-  }
   
   var loaders = [
           new ElementLoader(
                 document.getElementById("select1"), 
-                sleepSelectMapper,
+                selectMapper,
                 "resources/SelectElements.json"),
           new ElementLoader(
                 document.getElementById("select2"),
-                sleepSelectMapper,
+                selectMapper,
                 "resources/SelectElements2.json")];
   var formElem = document.getElementById("AjaxForm1");
   var formLoader = new FormLoader(formElem, loaders);
